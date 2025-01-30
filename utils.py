525127274564e14ca7aa8333ceb5ced8c85a00be
Dylan Prado <drwil1018@yahoo.com>
2025-01-29 18:59:28 -0600

    more bug fixes

--- utils.py
+++ utils.py
@@ -37,11 +37,17 @@ def select_deck(deck_index, base_url):
 def check_effects(base_url):
         response = requests.get(f"{base_url}/composition/clips/selected")
         if response.status_code == 200:
-            data = response.json()
-            effects = data.get('video', {}).get('effects', [])
-            
-            if len(effects) > 1:
-                if effects[1].get('name') == 'Exposure':
-                    expose_value = round(effects[1].get('params', {}).get('Exposure', {}).get('value'), 2)
-                    return expose_value
+            try:
+                data = response.json()
+                effects = data.get('video', {}).get('effects', [])
+                
+                if len(effects) > 1:
+                    if effects[1].get('name') == 'Exposure':
+                        expose_value = round(effects[1].get('params', {}).get('Exposure', {}).get('value'), 2)
+                        return expose_value
+                    
+                    else:
+                        return None
+            except:
+                return None
                     
